46a265930d701ab3b51b5583f141febd9d664a7c
ankit-raj-KR <ankit.raj@kroger.com>
2024-08-29 00:37:35 +0530

    changed the server socket port to 4221

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 public class Main {
   public static void main(String[] args) {
     try{
-        ServerSocket serverSocket = new ServerSocket(4123); // this creates a socket on the server, and then the server is listening on  port 4122
+        ServerSocket serverSocket = new ServerSocket(4221); // this creates a socket on the server, and then the server is listening on  port 4122
         serverSocket.setReuseAddress(true);
         Socket clientSocket = serverSocket.accept(); // now the server will wait for the client socket to request a connection, when the client request the server
         // form a connection and returns the socket, remember this is a blocking operation.
