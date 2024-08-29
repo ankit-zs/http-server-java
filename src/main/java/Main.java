7f436584cea33359b5dcc67f4494c28687f92af0
ankit-raj-KR <ankit.raj@kroger.com>
2024-08-29 12:24:52 +0530

    implemented respond with body and refactored code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,37 +17,67 @@ public class Main {
         InputStream inputStream = clientSocket.getInputStream(); // it is the stream of data that client sent us as input to the server
         OutputStream outputStream = clientSocket.getOutputStream();  // it is the stream of data that client will receive as output from the server
 
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream); // this is used to convert bytes of stream into charactesr
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // this is used to convert bytes of stream into characters but using buffers, it means
-        // that instead of performing reading operation for each character, instead we save those characters into a internal buffer, then read a group of character, due
-        // to this we have to do much less reading operation and thus improving performance.
+        String request = getRequestFromClient(inputStream);
 
-        String request = bufferedReader.readLine(); // used to read the entire line from the input stream at once.
-
-        String[] splitEntireRequestWithCRLF = request.split("\r\n");
-        String requestLine = splitEntireRequestWithCRLF[0];
-        String[] splitRequestLineWithSpace = requestLine.split(" ");
-
-        String requestTarget = splitRequestLineWithSpace[1];
-
-        if(requestTarget.equals("/")){
-            System.out.println("response is: " + "HTTP/1.1 200 OK\r\n\r\n");
-            outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-        }
-        else{
-            System.out.println("response is: " + "HTTP/1.1 404 Not Found\r\n\r\n");
-            outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        if(request != null) {
+//            ExtractUrlPath(request, outputStream);
+            ResponseWithBody(request, outputStream);
         }
 
         // where \r\n is CRLF i.e. carriage return(\r) line feed(\n)
         // where \r means the cursor moves to the starting of the current line
         // where \n means that the cursor moves to the next line without moving to the start of the next line
         // when we do \r\n then the cursor first moves to the start of the current line then moves to the next, so as result moves to the start of the next line
-
-
     }
     catch(IOException e){
         System.err.println("exception occurred in server socket: " +  e.getMessage());
     }
   }
+
+  private static String getRequestFromClient(InputStream inputStream){
+      try{
+          InputStreamReader inputStreamReader = new InputStreamReader(inputStream); // this is used to convert bytes of stream into charactesr
+          BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // this is used to convert bytes of stream into characters but using buffers, it means
+          // that instead of performing reading operation for each character, instead we save those characters into a internal buffer, then read a group of character, due
+          // to this we have to do much less reading operation and thus improving performance.
+
+          String request = bufferedReader.readLine(); // used to read the entire line from the input stream at once.
+          return request;
+      }
+      catch (IOException e){
+          System.err.println("Exception occurred while getting request from client: " + e.getMessage());
+          return null;
+      }
+  }
+
+  private static void ExtractUrlPath(String request, OutputStream outputStream) {
+      try{
+          String urlPath = request.split(" ")[1];
+          if(urlPath.equals("/")){
+              System.out.println("HTTP/1.1 200 OK\r\n\r\n");
+              outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+          }
+          else{
+              System.out.println("HTTP/1.1 404 Not Found\r\n\r\n");
+              outputStream.write(("HTTP/1.1 404 Not Found\r\n\r\n").getBytes());
+          }
+      }
+      catch (IOException e){
+          System.err.println("exception occurred in extracting url path from the request: " + e.getMessage());
+      }
+  }
+
+  private static void ResponseWithBody(String request, OutputStream outputStream){
+      try{
+          String requestTarget = request.split(" ")[1];
+          String[] requestTargetChunks = requestTarget.split("/");
+          String requestTargetUrlLast = requestTargetChunks[requestTargetChunks.length - 1];
+          outputStream.write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "
+                  + requestTargetUrlLast.length() + "\r\n\r\n" + requestTargetUrlLast).getBytes());
+      }
+      catch (IOException e){
+          System.err.println("Exception occurred in ResponseWithBody: " + e.getMessage());
+      }
+  }
+
 }
